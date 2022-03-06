71c1df81638ef04d0e7f21caa4c34259adeaedaa
mking <zhb888999@126.com>
2022-03-06 22:56:37 +0000

    fix ranger install pacman not sudo

--- sub-unit-install/fzf.sh
+++ sub-unit-install/fzf.sh
@@ -3,7 +3,7 @@
 function install_fzf() {
     [ -n "${INSTALL_FZF+1}" ] && return
 
-    sudo pacman -S fzf the_silver_searcher 
+    sudo pacman $PACMAN_FLAGS -S fzf the_silver_searcher 
     cp -r config/fzf/. ~/.fzf
     mv ~/.fzf/fzf.bash ~/.fzf.bash
     mv ~/.fzf/fzf.zsh ~/.fzf.zsh
