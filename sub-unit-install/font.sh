83cccd58af47012ec99e1136493d7b803ce4a06a
mking <zhb888999@126.com>
2022-03-07 21:16:24 +0000

    install font from github

--- sub-unit-install/font.sh
+++ sub-unit-install/font.sh
@@ -5,8 +5,10 @@ function install_font() {
 
     sudo pacman $PACMAN_FLAGS -S adobe-source-han-serif-otc-fonts ttf-fira-code ttf-nerd-fonts-symbols
     mkdir -p ~/Github
-    # git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/Github/nerd-fonts
-    # bash ~/Github/nerd-fonts/install.sh
+    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/Github/nerd-fonts
+    cd ~/Github/nerd-fonts/
+    bash install.sh
+    cd -
 
     export INSTALL_FONT=1
 }
