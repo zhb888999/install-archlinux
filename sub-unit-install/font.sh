d4f419b04b3351556ef6834c32e038778910dff9
mking <zhb888999@126.com>
2022-03-08 00:59:54 +0000

    fix font

--- sub-unit-install/font.sh
+++ sub-unit-install/font.sh
@@ -3,7 +3,7 @@
 function install_font() {
     [ -n "${INSTALL_FONT+1}" ] && return
 
-    sudo pacman $PACMAN_FLAGS -S adobe-source-han-serif-otc-fonts ttf-fira-code ttf-nerd-fonts-symbols
+    sudo pacman $PACMAN_FLAGS -S adobe-source-han-serif-otc-fonts ttf-fira-code ttf-nerd-fonts-symbols powerline-fonts
     mkdir -p ~/.local/share/fonts
     tar zxvf font/FiraCodeNerdFont.tar.gz -C ~/.local/share/fonts
     export INSTALL_FONT=1
